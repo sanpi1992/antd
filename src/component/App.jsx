c6bbe882ba83e57958bcde209ecce02d80b3e011
YunChao <974753530@qq.com>
2016-03-08 13:38:17 +0800

    创建创建项目“

---
+++ src/component/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { DatePicker } from 'antd';
+
+class App extends Component {
+  render() {
+    return <DatePicker />;
+  }
+}
+
+export default App;
+
